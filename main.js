e82f3c5968dc4c6ebaa24924c5686eb590e26152
Hannah Kang <hkang1@andrew.cmu.edu>
2018-10-04 17:19:04 -0400

    Much done pls fail me

--- main.js
+++ main.js
@@ -1,7 +1,20 @@
-function addNewList() {
-    alert('hello world!');
+function deleteListItem(item) {
+    item.parentNode.removeChild(item);
+}
+
+function completeListItem(item){
+	if(item.checked){
+		item.parentNode.style.textDecoration = "line-through";
+	} else {
+        item.parentNode.style.textDecoration = "none";
+	}
 }
 
 function addListItem() {
-    console.log('hello world');
+	var x = document.getElementById("grocery-list");
+	var elem = document.createElement("li")
+	var y = document.getElementById("inp")
+	elem.innerHTML = '<input type="checkbox" class="complete-item" onclick="completeListItem(this)">'+y.value + '<button class="delete-item" onclick="deleteListItem(this.parentNode)">X</button>'
+	x.appendChild(elem)
+    // console.log('hello world');
 }
